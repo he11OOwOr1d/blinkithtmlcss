b577763239390db90c1ba8411f30dce9dd6ee6dd
Nikhil Raj <nikhilraj@Nikhils-MacBook-Pro.local>
2025-04-10 22:25:10 +0530

    added click

--- index.js
+++ index.js
@@ -26,40 +26,23 @@ logoutbutton.addEventListener('click',()=>{
   localStorage.removeItem('loginarr')
 })
 
-acc.addEventListener('mouseover',(event)=>{
-  console.log("this is it : ",parseStd.data['namehe'])
-  let {username,emails,pass}=parseStd.data
-  // console.log('clicked')
+acc.addEventListener('click', () => {
+  console.log('click')
   let tooltipDiv = document.createElement('div');
-    tooltipDiv.innerHTML = `
-      <p>Name: ${parseStd.data['namehe']}</p>
-      <p>Email: ${parseStd.data['email']}</p>
-      <p>My Orders</p>
-      <p>Saved Address</p>
-      <p>FAQ</p>
-      <p>Account Privacy</p>
-    `;
-    
-    // Add some basic styling to make the tooltip look nice
-    tooltipDiv.style.position = 'absolute';
-    tooltipDiv.style.backgroundColor = 'black';
-    tooltipDiv.style.color = 'white';
-    tooltipDiv.style.padding = '5px';
-    tooltipDiv.style.borderRadius = '3px';
-    
-    // Position the tooltip near the mouse
-    tooltipDiv.style.left = `${event.pageX + 10}px`;
-    tooltipDiv.style.top = `${event.pageY + 10}px`;
-    
-    // Add a class for potential additional styling
-    tooltipDiv.classList.add('tooltip');
-    
-    // Append the tooltip to the body
-    document.body.appendChild(tooltipDiv);
-    
-    // Remove the tooltip when the mouse leaves
-    acc.addEventListener('mouseout', () => {
-        document.body.removeChild(tooltipDiv);
-    }, { once: true });
-})
+  tooltipDiv.innerHTML = `
+    <p>Name: ${parseStd.data['namehe']}</p>
+    <p>Email: ${parseStd.data['email']}</p>
+    <p>My Orders</p>
+    <p>Saved Address</p>
+    <p>FAQ</p>
+    <p>Account Privacy</p>
+  `;
+  
+  
+});
 
+// let textdiv = document.createElement('div')
+// let info = document.createElement('p')
+// info.innerText = 'hi'
+// textdiv.appendChild(p)
+// hidiv.appendChild(textdiv)
